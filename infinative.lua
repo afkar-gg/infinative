e9de18a4e496c84b430d45b721a45361b76a957b
afkar-gg <afkarsyarif45@gmail.com>
2025-05-22 20:08:14 +0700

    Update infinative.lua

--- infinative.lua
+++ infinative.lua
@@ -3,9 +3,9 @@ thescriptkey2 = "ooaGUehvdVoSOdeqlMYPLhcLjjdULWJb"
 
 coin_flip=math.random(0, 1)
 if coin_flip==0 then
-    sript_key=thescriptkey2;
+    script_key=thescriptkey2;
 (loadstring or load)(game:HttpGet("https://getnative.cc/script/loader"))()
 else
-    sript_key=thescriptkey1;
+    script_key=thescriptkey1;
 (loadstring or load)(game:HttpGet("https://getnative.cc/script/loader"))()
 end
